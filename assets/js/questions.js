56c7955fa649d3e9f31e4eafc9c0c4104b31ea10
Hayley <hb_060492@hotmail.co.uk>
2023-01-14 16:19:51 +0000

    edited the wording of some questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,32 +2,27 @@
 
 var questionObj = [
     {
-        num: 1,
         question: "Commonly used data types DO NOT include ____",
         choices: ["strings", "booleans", "alerts", "numbers"],
         correctAnswer: "alerts"
     },
     {
-        num: 2,
-        question: "The condition in an if/else statement is enclosed within ____",
+        question: "The condition in an if / else statement is enclosed within ____",
         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
         correctAnswer: "parentheses"
     },
     {
-        num: 3,
         question: "Arrays in Javascript can be used to store:",
         choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
         correctAnswer: "all of the above"
     },
     {
-        num: 4,
-        question: "String values must be enclosed within ____ when being assigned variables:",
+        question: "String values must be enclosed within ____ when being assigned to variables:",
         choices: ["commas", "curly brackets", "quotes", "parentheses"],
         correctAnswer: "quotes"
     },
     {
-        num: 5,
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        question: "A very useful tool used during development for printing content to the debugger is:",
         choices: ["javascript", "terminal/bash", "for loops", "console log"],
         correctAnswer: "console log"
     },
